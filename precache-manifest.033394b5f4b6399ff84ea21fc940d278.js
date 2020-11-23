fea74e1ec488f4f25b477fce134a53d13e1a90ea
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2020-11-23 01:49:37 +0100

    Updates

---
+++ precache-manifest.033394b5f4b6399ff84ea21fc940d278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe2cbe6b2cdf872a02826957f2a9ffc",
+    "url": "/youtubeApplication/index.html"
+  },
+  {
+    "revision": "d73f9670cbfe13531012",
+    "url": "/youtubeApplication/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ba999b65220faed986b0",
+    "url": "/youtubeApplication/static/css/main.9ee2cbe7.chunk.css"
+  },
+  {
+    "revision": "d73f9670cbfe13531012",
+    "url": "/youtubeApplication/static/js/2.f339f09d.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/youtubeApplication/static/js/2.f339f09d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba999b65220faed986b0",
+    "url": "/youtubeApplication/static/js/main.0446be98.chunk.js"
+  },
+  {
+    "revision": "b6321d9b384f1f171c04",
+    "url": "/youtubeApplication/static/js/runtime-main.a5248477.js"
+  }
+]);
